6a1450f7ae50217eac84e2baf0d918a798811578
Yelro <vanorley20@gmail.com>
2023-11-15 17:18:49 -0500

    Fix test

--- src/index.ts
+++ src/index.ts
@@ -12,4 +12,4 @@ export const subtract = (a: number, b: number) => {
   return a - b;
 };
 
-export const multiply = (a: number, b: number) => a / b;
+export const multiply = (a: number, b: number) => a * b;
