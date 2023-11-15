28231661cf9dccc473393f0bc713a26af581d997
Yelro <vanorley20@gmail.com>
2023-11-15 16:56:40 -0500

    No ticket: multiplicar

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,7 +1,19 @@
-import { sum } from '../src';
+import { sum, subtract, multiply } from '../src';
 
 describe('blah', () => {
   it('works', () => {
     expect(sum(1, 1)).toEqual(2);
+  });
+});
+
+describe('blah', () => {
+  it('works', () => {
+    expect(subtract(3, 1)).toEqual(2); //3-1=2
+  });
+});
+
+describe('multiply', () => {
+  it('should multiply two numbers', () => {
+    expect(multiply(2, 2)).toEqual(4);//2*2=4
   });
 });
