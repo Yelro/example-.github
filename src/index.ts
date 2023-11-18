619da4b9f481ba3edc21ad7ba5a3a5d10aecff37
Yelro <vanorley20@gmail.com>
2023-11-18 08:28:06 -0500

    no ticket/calculator-capitalize-index

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
 export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
   return a + b;
 };
 
 export const subtract = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
   return a - b;
 };
 
-export const multiply = (a: number, b: number) => a * b;
+export const multiply = (a: number, b: number) => {
+  return a * b;
+};
+
+export const divide = (a: number, b: number) => {
+  return a / b;
+};
+
+export const capitalize = (str: string) => {
+  if (typeof str !== 'string') return 'Please provide a string';
+
+  return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
+};
+
+export const stringLength = (str: string) => {
+  const isValid = str.length >= 1 && str.length <= 10;
+  if (!isValid) return 'invalid string length';
+
+  return str.length;
+};
+
+export const reverseString = (str: string) => str.split('').reverse().join('');
--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,13 +1,13 @@
-import { sum, subtract, multiply } from '../src';
+import { sum, subtract, multiply, divide, capitalize, stringLength, reverseString } from '../src';
 
-describe('blah', () => {
-  it('works', () => {
+describe('sum', () => {
+  it('should sum two numbers', () => {
     expect(sum(1, 1)).toEqual(2);
   });
 });
 
-describe('blah', () => {
-  it('works', () => {
+describe('subtract', () => {
+  it('should subtract two numbers', () => {
     expect(subtract(3, 1)).toEqual(2);
   });
 });
@@ -15,5 +15,29 @@ describe('blah', () => {
 describe('multiply', () => {
   it('should multiply two numbers', () => {
     expect(multiply(2, 2)).toEqual(4);
+  });
+});
+
+describe('divide', () => {
+  it('should divide two numbers', () => {
+    expect(divide(4, 2)).toEqual(2);
+  });
+});
+
+describe('capitalize', () => {
+  it('should change to capital letter', () => {
+    expect(capitalize('orley')).toBe('Orley');
+  });
+});
+
+describe('stringLength', () => {
+  it('should give the string length', () => {
+    expect(stringLength('Orley')).toBe(5);
+  });
+});
+
+describe('reverseString', () => {
+  it('should reversing a string', () => {
+    expect(reverseString('Orley')).toBe('yelrO');
   });
 });
